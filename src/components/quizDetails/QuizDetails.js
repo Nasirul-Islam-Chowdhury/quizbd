378a8432a31d2f1e8435673f0784e2c112f997e9
nicchy123 <nicchy321@gmail.com>
2023-02-25 18:48:45 +0600

    test

--- src/components/quizDetails/QuizDetails.js
+++ src/components/quizDetails/QuizDetails.js
@@ -8,11 +8,23 @@ const QuizDetails = () => {
     const questions = details.questions;
     return (
         <div className='text-slate-800 bg-slate-100 py-5'>
-            <h1 className='text-center text-3xl font-bold pb-5'>Quiz of {details.name}</h1>
-            {
-            questions.map(quesion => <Question key={quesion.id} details={details} questions={questions} quesion={quesion}></Question>)
-       }
+     <div className='text-xl font-semibold pl-5 sticky text-end pr-2'>
+        <h1>total correct answer <span id='cor'>00</span></h1>
+        <h1>total correct answer <span>00</span></h1>
+      </div>
+<div>
+    
+   <h1 className='text-center text-3xl font-bold pb-5'>Quiz of {details.name}</h1>
+     
+     <div>
+     {
+          questions.map(quesion => <Question key={quesion.id} details={details} questions={questions} quesion={quesion}></Question>)
+     }
+     </div>
+</div>
+
         </div>
+    
       
     );
 };
--- src/components/result/result.js
+++ src/components/result/result.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Swal from "sweetalert2"
 
-export const result = (quesion, option) => {
+export const Result = (quesion, option) => {
+    const [marks, setMarks] = useState(1);
     if (quesion.correctAnswer === option) {
         Swal.fire(" Congratulatios! Correct Answer")
+       document.getElementById("cor").innerText= setMarks(marks);
+     
+       console.log("marks")
     } else {
-        Swal.fire("Opps! Wrong Answer")
+        Swal.fire("Opps! Wrong Answer");
     }
 }
 
